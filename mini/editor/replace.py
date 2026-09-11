330e7c8f080b25f7fddce185e5ea772323086ce2
Tibor Hajszan <thajszan@t-online.hu>
2026-09-11 14:09:49 +0200

    Local File Editor > replace.py > streamlined main logic.

--- mini/editor/replace.py
+++ mini/editor/replace.py
@@ -10,7 +10,7 @@ def apply_edits(
     content_list: list[str],
     search_index: tuple[int,int],
     replace_list: list[str],
-    target_path: str
+    local_path: str
 ) -> dict[str, int | str]:
     """
     Replaces SEARCH lines with REPLACE lines in the local file.
@@ -18,7 +18,7 @@ def apply_edits(
     - *content_list* > local file content as list of lines
     - *search_index* > SEARCH block slice indices as tuple
     - *replace_list* > REPLACE block content as list of lines
-    - *target_path* > path to local file as string
+    - *local_path* > path to local file as string
     #### Returns:
     - *dict* > status code, status message
     """
@@ -28,25 +28,25 @@ def apply_edits(
     #>> content list is verified upstream
     #>> search index is verified upstream
     #>> replace list is verified upstream
-    #>> target path is verified upstream
+    #>> local path is verified upstream
 
-    ### converting target path str >>> path
-    target_file: Path = Path(target_path)
+    ### converting local path str >>> path
+    local_file: Path = Path(local_path)
     
     ### function main logic --------------------------------------------------------------------------------------------
 
-    ### copying content list | applying changes
+    ### copying content list | executing lines replace
     modified_content: list[str] = content_list[:]
     modified_content[search_index[0]:search_index[1]] = replace_list
 
     ### rebuilding raw file content
     raw_content: str = "\n".join(modified_content) + "\n" if modified_content else ""
 
-    ### writing to target file | handling file write errors
+    ### writing local file | handling file writing errors
     try:
-        target_file.write_text(data=raw_content, encoding="utf-8", newline=None)
+        local_file.write_text(data=raw_content, encoding="utf-8", newline=None)
     except Exception as error:
-        return {"status": -1, "message": f"File Write Failure > {target_path}:\n{error}"}
+        return {"status": -1, "message": f"File Writing Failure > {local_path}:\n{error}"}
     
     ### function ends //////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -55,4 +55,4 @@ def apply_edits(
 
 ### manual testing block ###############################################################################################
 if __name__ == "__main__":
-    print("\n[!] Diff Edit Executor > No Test Specified\n")
+    print("\n[!] Replace Executor > No Test Specified\n")
